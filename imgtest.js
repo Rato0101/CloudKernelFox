8f5436d215220012b272e1ac27bec726240d9512
CloudKernelFox <rato23032215@gmail.com>
2026-08-02 19:09:43 +0100

    adadas

---
+++ imgtest.js
@@ -0,0 +1,17 @@
+const style = `
+  body {
+    padding: 3em;
+  }
+
+  img {
+    transition: transform .5s;
+  }
+
+  img:hover {
+    transform: scale(2.5);
+  }
+`;
+
+const styleSheet = document.createElement('style');
+styleSheet.textContent = style;
+document.head.appendChild(styleSheet);
